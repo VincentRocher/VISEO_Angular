9613bc093fe850a78867ec5063a2eb2ba34fd9f2
Vincent <vincent.rocher@viseo.com>
2015-04-24 13:38:55 +0200

    ajout du fichier data.js pour la BDD

---
+++ ProjetAngularClean/data.js
@@ -0,0 +1,168 @@
+[{
+    "id" : 0,
+    "Titre" : "javascript",
+    "questions" : [
+        {
+
+            "id" : 0,
+            "Titre" : "Comment selectionner une div en javascript?",
+            "reponses" : [
+                {
+                    "id" : 0,
+                    "Titre" : "GetElementById(#id);",
+                    "isTrue" : true
+                },
+                {
+                    "id" : 1,
+                    "Titre" : "</br>",
+                    "isTrue" : false
+                },
+                {
+                    "id" : 2,
+                    "Titre" : "$(#id)",
+                    "isTrue" : false
+                }
+            ]
+        },
+        {
+            "id" : 1,
+            "Titre" : "Comment faire apparaitre un message d'alerte?",
+            "reponses" : [
+                {
+                    "id" : 0,
+                    "Titre" : "OnStart()",
+                    "isTrue" : false
+                },
+                {
+                    "id" : 1,
+                    "Titre" : "messageAlerte()",
+                    "isTrue" : false
+                },
+                {
+                    "id" : 2,
+                    "Titre" : "alert()",
+                    "isTrue" : true
+                }
+            ]
+        },
+        {
+            "id" : 2,
+            "Titre" : "3?",
+            "reponses" : [
+                {
+                    "id" : 0,
+                    "Titre" : "OnStart()",
+                    "isTrue" : false
+                },
+                {
+                    "id" : 1,
+                    "Titre" : "messageAlerte()",
+                    "isTrue" : false
+                },
+                {
+                    "id" : 2,
+                    "Titre" : "alert()",
+                    "isTrue" : true
+                }
+            ]
+        }
+    ]
+},
+    {
+        "id" : 1,
+        "Titre" : "HTML5",
+        "questions" : [
+            {
+                "_id":'41224d776a326fb40f000004',
+                "id" : 0,
+                "Titre" : "quelle balise pour faire un header?",
+                "reponses" : [
+                    {
+                        "id" : 0,
+                        "Titre" : "<header/>",
+                        "isTrue" : false
+                    },
+                    {
+                        "id" : 1,
+                        "Titre" : "<header></header>",
+                        "isTrue" : true
+                    },
+                    {
+                        "id" : 2,
+                        "Titre" : "::header::",
+                        "isTrue" : false
+                    }
+                ]
+            },
+            {
+                "id" : 1,
+                "Titre" : "comment aller a la ligne?",
+                "reponses" : [
+                    {
+                        "id" : 0,
+                        "Titre" : "<br/>",
+                        "isTrue" : true
+                    },
+                    {
+                        "id" : 1,
+                        "Titre" : "/n",
+                        "isTrue" : false
+                    },
+                    {
+                        "id" : 2,
+                        "Titre" : "entrée",
+                        "isTrue" : false
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id" : 2,
+        "Titre" : "Angular",
+        "questions" : [
+            {
+                "id" : 0,
+                "Titre" : "Comment cacher un element?",
+                "reponses" : [
+                    {
+                        "id" : 0,
+                        "Titre" : "ng-hide;",
+                        "isTrue" : true
+                    },
+                    {
+                        "id" : 1,
+                        "Titre" : "ng-show",
+                        "isTrue" : false
+                    },
+                    {
+                        "id" : 2,
+                        "Titre" : "ng-cacher",
+                        "isTrue" : false
+                    }
+                ]
+            },
+            {
+                "id" : 1,
+                "Titre" : "qu'utiliser pour faire des ng-model dynamiques?",
+                "reponses" : [
+                    {
+                        "id" : 0,
+                        "Titre" : "scoop",
+                        "isTrue" : false
+                    },
+                    {
+                        "id" : 1,
+                        "Titre" : "noscope",
+                        "isTrue" : false
+                    },
+                    {
+                        "id" : 2,
+                        "Titre" : "scope",
+                        "isTrue" : true
+                    }
+                ]
+            }
+        ]
+    }
+]
