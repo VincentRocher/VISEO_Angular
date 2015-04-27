ca7e8a96ffb429c0b212bfc49e48c6f8b91015fc
Vincent <vincent.rocher@viseo.com>
2015-04-27 16:15:25 +0200

    update des tests unitaires: ajout de new/save/delete qcm et questions, et tests pour inscription/score/login

--- ProjetAngularClean/js/qcmSpec.js
+++ ProjetAngularClean/js/qcmSpec.js
@@ -40,12 +40,12 @@ describe('loginController', function() {
 
 });
 
-describe('QCM', function(){
+describe('Inscription et Score controller', function(){
 
     beforeEach(module('QCM'));
     describe("score test", function(){
 
-        var $ctrl, httpback,$loc,score, login;
+        var $ctrl, httpback,$loc,score, login, title, question;
 
         beforeEach(function(){
 
@@ -67,12 +67,143 @@ describe('QCM', function(){
             var score = $ctrl('scoreController', {$scope: $scope, $routeParams:{qcmId : 0}});
             score.RepQuest = [0,1,2];
             score.connectedUserId = "06684867";
-            score.evaluate()
+            score.evaluate();
             //inscript.inscription().then(function(res){result = res;})
             httpback.flush();
             //console.log(score.score.value);
             expect(score.score.value).toBeDefined();
             expect(score.score.value).toBe("validated");
+            //expect(result).toBe("validated");
+
+        })
+        it("Should create Question and delete editedQuestion", function(){
+            httpback.expectPOST("/rest/QCMList/0/QuesList").respond({value: "validated"});
+            httpback.whenGET("./rest/QCMList").respond({value: "test"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            var question = $ctrl('questionsEditController', {$scope: $scope, $routeParams:{qcmId : 0}});
+            question.editedQuestion="EDITEEED";
+            question.RepQuest = [0,1,2];
+            question.connectedUserId = "06684867";
+            question.actualQcm={};
+            question.actualQcm.id=0;
+            question.actualQcm.questions=[0,1,4,2,5];
+            question.createQuestionInput={};
+            question.createQuestionInput.Titre = "test";
+            question.new();
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(question.editedQuestion).toBe(undefined);
+            console.log(question.newQues);
+            expect(question.newQues.value).toBe("validated");
+            //expect(result).toBe("validated");
+
+        })
+        it("Should create QCM and delete editedQcm", function(){
+            httpback.expectPOST("/rest/QCMList").respond({value: "validated"});
+            httpback.whenGET("./rest/QCMList").respond({value: "test"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            title = $ctrl('titlesEditController', {$scope: $scope, $routeParams:{qcmId : 0}});
+            $scope.qcmTable = [0,1,2];
+            title.editedQcm="EDITEEED";
+            title.RepQuest = [0,1,2];
+            title.connectedUserId = "06684867";
+            title.actualQcm={};
+            title.actualQcm.id=0;
+            title.actualQcm.questions=[0,1,4,2,5];
+            title.createQcmInput="plop";
+            title.new();
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(title.editedQcm).toBe(undefined);
+            expect(title.newQCM.value).toBe("validated");
+            //expect(result).toBe("validated");
+
+        })
+        it("Should Delete QCM and editedQcm", function(){
+            httpback.expectDELETE("/rest/QCMList/1").respond({value: "validated"});
+            httpback.whenGET("./rest/QCMList").respond({value: "test"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            title = $ctrl('titlesEditController', {$scope: $scope, $routeParams:{qcmId : 0}});
+            $scope.qcmTable = [0,1,2];
+            title.editedQcm="EDITEEED";
+            title.RepQuest = [0,1,2];
+            title.connectedUserId = "06684867";
+            title.actualQcm={};
+            title.actualQcm.id=0;
+            title.actualQcm.questions=[0,1,4,2,5];
+            title.createQcmInput="plop";
+            title.delete(1);
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(title.editedQcm).toBe(undefined);
+            //expect(result).toBe("validated");
+
+        })
+        it("Should Edit QCM and delete editedQcm", function(){
+            httpback.whenGET("/rest/QCMList/1").respond({value: "test"});
+            httpback.whenGET("./rest/QCMList").respond({value: "validated"});
+            httpback.expectPOST("/rest/QCMList/1").respond({value: "validated"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            title = $ctrl('titlesEditController', {$scope: $scope, $routeParams:{qcmId : 0}});
+            $scope.qcmTable = [0,1,2];
+            title.editedQcm="EDITEEED";
+            title.qcmEdit = [];
+            title.qcmEdit[1]={};
+            title.qcmEdit[1].Titre="test";
+            title.RepQuest = [0,1,2];
+            title.connectedUserId = "06684867";
+            title.actualQcm={};
+            title.actualQcm.id=0;
+            title.actualQcm.questions=[0,1,4,2,5];
+            title.createQcmInput="plop";
+            title.save(1);
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(title.editedQcm).toBe(undefined);
+            //expect(result).toBe("validated");
+
+        })
+        it("Should Delete Question and editedQuestion", function(){
+            httpback.expectDELETE("/rest/QCMList/0/QuesList/0").respond({value: "validated"});
+            httpback.whenGET("./rest/QCMList").respond({value: "test"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            var question = $ctrl('questionsEditController', {$scope: $scope, $routeParams:{qcmId : 0}});
+            question.editedQuestion="EDITEEED";
+            question.RepQuest = [0,1,2];
+            question.connectedUserId = "06684867";
+            question.actualQcm={};
+            question.actualQcm.id=0;
+            question.actualQcm.questions=[0,1,4,2,5];
+            question.delete(0);
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(question.editedQuestion).toBe(undefined);
+            console.log(question.newQues);
+            //expect(result).toBe("validated");
+
+        })
+        it("Should Edit Question and editedQuestion", function(){
+            httpback.expectGET("/rest/QCMList/0/QuesList/0").respond({value: "validated"});
+            httpback.whenGET("./rest/QCMList").respond({value: "test"});
+            httpback.whenGET("./view/titres.html").respond({value: "test"});
+            var question = $ctrl('questionsEditController', {$scope: $scope, $routeParams:{qcmId : 0, quesId:0}});
+            question.editedQuestion="EDITEEED";
+            question.RepQuest = [0,1,2];
+            question.connectedUserId = "06684867";
+            question.actualQcm={};
+            question.actualQcm.id=0;
+            question.actualQcm.questions=[0,1,4,2,5];
+            question.save(0);
+            //inscript.inscription().then(function(res){result = res;})
+            httpback.flush();
+            //console.log(score.score.value);
+            expect(question.editedQuestion).toBe(undefined);
+
             //expect(result).toBe("validated");
 
         })
@@ -96,21 +227,66 @@ describe('QCM', function(){
             expect($loc.path()).toBe("/index");
 
         })
+        it("mode Normal click on QCM not cleared=> redirect to inscription", function(){
+            $loc.path("./splotch");
+            title = $ctrl('titlesController', {$scope: $scope});
+            title.goQcm(0);
+            expect($loc.path()).toBe("/inscription/0");
 
+        })
+        it("mode Normal click on QCM cleared=> redirect to inscription", function(){
+            $loc.path("./splotch");
+            title = $ctrl('titlesController', {$scope: $scope});
+            title.qcm=[];
+            title.qcm[0]=true;
+            title.goQcm(0);
+            expect($loc.path()).toBe("/qcm/0/numq/0");
 
+        })
+        it("Click on button to move question further, or question in QCM cleared", function(){
+            $loc.path("./splotch");
+            question = $ctrl('questionController', {$scope: $scope, $routeParams: {qcmId: 0, quesId:2}});
+            question.actualQcm = {id : 0};
+            question.QIndex = 2;
+            question.moveQ(1);
+            expect($loc.path()).toBe("/qcm/0/numq/3");
+        })
+        it("click on QCM when qcm is finished => should put QCM to Cleared and redirect to score", function(){
+            $loc.path("./splotch");
+            question = $ctrl('questionController', {$scope: $scope, $routeParams: {qcmId: 0, quesId:2}});
 
-    })
-    describe('login test', function(){
-        var $ctrl, $loc,login;
+            question.actualQcm = [];
+            question.actualQcm.id = 0;
+            question.actualQuestion = [];
+            question.actualQuestion.Repondu = true;
+            question.actualQcm.Repondu = "";
+            question.QIndex = 2;
+            question.qcmComplete = true;
+            question.nextQ();
+            expect(question.qcm[0].cleared).toBeTruthy();
+            expect($loc.path()).toBe("/qcm/0/score");
 
-        beforeEach(function(){
+        })
+        it("click on QCM when qcm isnt finished => should go next question and fill table RepQuest", function(){
+            $loc.path("./splotch");
+            question = $ctrl('questionController', {$scope: $scope, $routeParams: {qcmId: 0, quesId:2}});
 
+            question.actualQcm = [];
+            question.RepQuest = [];
+            question.actualQcm.id = 0;
 
+            question.actualQcm.questions = [1,1,1,1];
+            question.actualQuestion = [];
+            question.actualQuestion.Repondu = "";
+            question.actualQuestion.id = 2;
+            question.actualQuestion.reponses = [0,0,0,1,4,1];
+            question.actualQcm.Repondu = "";
+            question.QIndex = 2;
+            question.nextQ(2, false);
+            expect($loc.path()).toBe("/qcm/0/numq/3");
+            expect(question.RepQuest[2]).toBe(2);
 
-
-
-
-        });
+        })
 
 
     })
